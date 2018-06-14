e5a98a4c23b9056257eee2908453b5823af8d1bf
edocit <edo.citta@gmail.com>
2018-06-15 00:20:27 +0200

    class Room
    Aggiunta la classe Room

--- Booking/bookingApp/models.py
+++ Booking/bookingApp/models.py
@@ -2,37 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-class Person(models.Model):
-    name = models.CharField(max_length=50)
-    surname = models.CharField(max_length=50)
-    date = models.DateField()
-    cf = models.CharField(max_length=20)
-    email = models.EmailField(max_length=100)
-
-class User(Person):
-    #to do carta di credito
-
-class HotelKeeper(Person):
-    userName = models.CharField(max_length=50)
-    password = models.PasswordInput(max_length=50)
-
-class RegisteredUser(User):
-    userName = models.CharField(max_length=50)
-    password = models.PasswordInput(max_length=50)
-
-class Hotel(models.Model):
-    name = models.CharField(max_length=30)
-    description = models.TextField()
-    city = models.CharField(max_length=20)
-    hotelKeeperId = models.ForeignKey(HotelKeeper,on_delete=models.CASCADE)
-
-class Booking(models.Model):
-    customerId = models.ForeignKey(User)
-    roomId = models.ForeignKey(Room)
-
-    #### Lasciare anche orario o solo data? ###
-    checkIn = models.DateTimeField()
-    checkOut = models.DateTimeField()
 
 class CreditCard(models.Model):
     number = models.IntegerField()
@@ -40,9 +9,57 @@ class CreditCard(models.Model):
     expirationMonth = models.IntegerField()
     cvvCode = models.IntegerField()
 
+
 class Address(models.Model):
     street = models.CharField(max_length=100)
     houseNumber = models.IntegerField()
     city = models.CharField(max_length=30)
     zipCode = models.CharField(max_length=15)
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    date = models.DateField()
+    cf = models.CharField(max_length=20)
+    email = models.EmailField(max_length=100)
+
+
+class User(Person):
+    #TODO CreditCard
+
+
+class HotelKeeper(Person):
+    userName = models.CharField(max_length=50)
+    password = models.PasswordInput(max_length=50)
+
+
+class RegisteredUser(User):
+    userName = models.CharField(max_length=50)
+    password = models.PasswordInput(max_length=50)
+
+
+class Hotel(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    city = models.CharField(max_length=20)
+    hotelKeeperId = models.ForeignKey(HotelKeeper, on_delete=models.CASCADE)
+    address = models.ForeignKey(Address,on_delete=models.CASCADE)
+
+
+class Room(models.Model):
+    capacity = models.IntegerField(default=0)
+    price = models.FloatField(default=0.0)
+    SERVICES = ( (0, "Minibar"), (1, "Telephone"), (2, "Breakfast") )
+    service = models.IntegerField(max_length=1, choices=SERVICES, default=None)
+    hotelId = models.ForeignKey(Hotel, on_delete=models.CASCADE)
+
+
+class Booking(models.Model):
+    customerId = models.ForeignKey(User)
+    roomId = models.ForeignKey(Room)
+    #### Lasciare anche orario o solo data? ###
+    checkIn = models.DateTimeField()
+    checkOut = models.DateTimeField()
+
 
