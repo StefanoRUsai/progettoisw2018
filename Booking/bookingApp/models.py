ac1e07e260b3fd287d4ee76fd7edeaa7c50c6a55
tatalessap <martapibirimp@gmail.com>
2018-06-14 18:43:42 +0200

    Classe Person

--- Booking/bookingApp/models.py
+++ Booking/bookingApp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Person(models.Model):
+    name= models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    date = models.DateField()
+    cf = models.CharField(max_length=20)
+    email = models.EmailField(max_length=100)
