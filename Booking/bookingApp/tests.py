070462332fb8440e441c01125cd578ab92609ad0
StefanoRUsai <stefano.r.usai@gmail.com>
2018-06-14 19:01:01 +0200

    modifica su test: test unitari classi

--- Booking/bookingApp/tests.py
+++ Booking/bookingApp/tests.py
@@ -1,9 +1,13 @@
 from django.test import TestCase
-
+import enum from Enum
 import unittest
 from .models import *
+
 
 class ModelTest(TestCase):
     def setUp(self):
         person = Person(name = 'Stefano', surname = 'Usai', email = 'susai@gmail.com', birthday = '30/10/1984', cf = 'SUASFN84R30B354E')
         person.save()
+
+        hotelKeeper = HotelKeeper ( name = 'Giorgia', surname = 'Campanile', email = 'giogio.com', birthday = '16/10/96', cf = 'CMPGRG96R56' )
+        hotelKeeper.save()
