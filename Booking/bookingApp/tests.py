aead24cd1889f52e9192338669c48ee29eb8b7c5
Giorgia Campanile <giorgia.campanile06@gmail.com>
2018-06-15 10:40:16 +0200

    Aggiunta test Hotel

--- Booking/bookingApp/tests.py
+++ Booking/bookingApp/tests.py
@@ -9,5 +9,10 @@ class ModelTest(TestCase):
         person = Person(name = 'Stefano', surname = 'Usai', email = 'susai@gmail.com', birthday = '30/10/1984', cf = 'SUASFN84R30B354E')
         person.save()
 
-        hotelKeeper = HotelKeeper ( name = 'Giorgia', surname = 'Campanile', email = 'giogio.com', birthday = '16/10/96', cf = 'CMPGRG96R56' )
+        hotelKeeper = HotelKeeper( name = 'Giorgia', surname = 'Campanile', email = 'giogio.com', birthday = '16/10/96', cf = 'CMPGRG96R56')
         hotelKeeper.save()
+
+        hotel = Hotel(name = 'T Hotel', description = 'Nel cuore di Cagliari...', town = 'Cagliari', OwnerID = 1)
+        hotel.save()
+
+        
