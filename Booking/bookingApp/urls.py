2b499846b6d46b6da6eb9411ffcb12323d62a22f
edocit <edo.citta@gmail.com>
2018-06-20 00:36:29 +0200

    Mapping URLs and views
    fatto il mapping delle funzioni views per la logica con i corrispondenti URLs per la web application

--- Booking/bookingApp/urls.py
+++ Booking/bookingApp/urls.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from django.urls import path
-from . import views
+from . import views as v
 
 urlpatterns = [
-    path('', views.addHotel),
+    path('', v.notRegisteredHome),
+    path('login/', v.loginPage),
+    path('signUp/', v.registerUser),
+    path('homeRegistered/', v.registeredUserHome),
+    path('search/', v.searchResults),
+    path('booking/', v.bookARoom),
+    path('home/', v.hotelKeeperHome), #ho lasciato home perchè dai disegni delle specifiche si vede così
+    path('hotels/', v.hotelsList), #ho lasciato hotels perchè dai disegni delle specifiche si vede così
+    path('addHotel', v.addHotel),
+    path('hotel/', v.hotelDetail),
+    path('addRoom/', v.addRoomToHotel),
+
 ]
