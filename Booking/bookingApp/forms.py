5e51c34e730409654dec72f042fa69f1d79edcf5
tatalessap <martapibirimp@gmail.com>
2018-06-19 19:24:37 +0200

    first form

--- Booking/bookingApp/forms.py
+++ Booking/bookingApp/forms.py
@@ -1,1 +1,12 @@
 from django import forms
+
+
+class AddHotelForm (forms.Form):
+    #maxdimensione, requisito necessario, adattamento html ni
+    name = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control"}))
+    description = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control"}))
+    street = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control"}))
+    houseNumber = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control"}))
+    city = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control"}))
+    zipCode = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control"}))
+    photoUrl = forms.ImageField(required=False, widget=forms.TextInput(attrs={"class": "form-control"}))
--- Booking/bookingApp/views.py
+++ Booking/bookingApp/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import AddHotelForm
+from .models import Hotel
 
 # Create your views here.
+def addHotel (request):
+    if request.method == 'POST':
+        form = AddHotelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            name = form.cleaned_data['name']
+            description = form.cleaned_data['description']
+            street = form.cleaned_data['street']
+            houseNumber = form.cleaned_data['houseNumber']
+            city = form.cleaned_data['city']
+            zipCode = form.cleaned_data['zipCode']
+            photoUrl = form.cleaned_data['photoUrl']
+
+            adress = Address(street, houseNumber, city, zipCode)
+
+            adress.save()
+
+            hotel = Hotel(name, description, adress, photoUrl)
+
+            hotel.save()
+
+            return redirect('manageHotel')
+    else:
+        form = AddHotelForm()
+
+
+    context = {'formAddHotel': form}
+    return render(request, 'addHotel.html', context)
