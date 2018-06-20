be5023a9b38f2434a7fc96ecb35254e4543d40c5
edocit <edo.citta@gmail.com>
2018-06-20 11:52:43 +0200

    view registerUser

--- Booking/bookingApp/views.py
+++ Booking/bookingApp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import AddHotelForm,AddRoomForm,formLogin
+from .forms import AddHotelForm,AddRoomForm,formLogin,registrationForm
 from .models import Hotel,Address,Room,IncludedService,RegisteredUser
 
 
@@ -82,12 +82,52 @@ def addRoomToHotel(request):
     return render(request, 'addRoom.html', context)
 
 
-
-def notRegisteredHome(request):
-    pass
+def isFreeUsername(toBeChecked):
+    for ut in RegisteredUser.objects.all():
+        if(ut.username == toBeChecked):
+            return False
+    return True
 
 
 def registerUser(request):
+    if (request.method == 'POST'):
+        form = registrationForm(request.POST)
+        if (form.is_valid() and isFreeUsername(str(form.cleaned_data['username']))):
+            # Setto in sessione le variabili relative all'utente loggato tranne la password
+            name = form.cleaned_data['nome']
+            surname = form.cleaned_data['cognome']
+            bd = form.cleaned_data['birthday']
+            codF = form.cleaned_data['cf']
+            emailAddr = form.cleaned_data['email']
+            userN = form.cleaned_data['username']
+            passW = form.cleaned_data['password']
+            rue = form.cleaned_data['street']
+            civicNr = form.cleaned_data['civicNumber']
+            userCity = form.cleaned_data['city']
+            cap = form.cleaned_data['zipCode']
+
+            #creo prima l'oggetto di tipo indirizzo
+            userAddr = Address(street=str(rue),houseNumber=str(civicNr),city=str(userCity),zipCode=str(cap))
+            Address.save()
+
+            #creo poi l'oggetto ut di tipo RegisteredUser che comprende l'oggetto userAddr creato poco sopra
+            ut = RegisteredUser(nome=str(name),cognome=str(surname),birthday=str(bd),cf=str(codF),email=str(emailAddr),username=str(userN),password=str(passW),address=userAddr)
+            ut.save()
+
+            request.session['usr'] = userN
+            return redirect('/homeRegisteredUser/')
+
+    else:  #Qui ci si entra in caso di prima di prima visualizzazione o richiesta GET
+        form = registrationForm()
+
+    context = {'form' : form}
+    return render(request,'signUp.html',context)
+
+
+
+
+
+def notRegisteredHome(request):
     pass
 
 def registeredUserHome(request):
